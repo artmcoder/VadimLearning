231fe6235ec3a0db0013ba63064011a9ae229193
Artem Yakunin <artm.yakunin@gmail.com>
2021-07-12 19:04:37 +0700

    first commit in lesson3

--- src/lesson2/interfaces/core/Fish.java
+++ src/lesson2/interfaces/core/Fish.java
@@ -1,8 +1,67 @@
 package lesson2.interfaces.core;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Fish implements Animal {
     @Override
     public void breath() {
         System.out.println("Рыба дышит жабрами");
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
